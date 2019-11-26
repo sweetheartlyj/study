e5f3070700bff4e01b067f76456b32220d90a64d
sweetheartlyj <54888015+sweetheartlyj@users.noreply.github.com>
2019-11-26 15:38:25 +0800

    Add files via upload

--- v-for.js
+++ v-for.js
@@ -2,9 +2,9 @@ var app = new Vue({
     el:'#app',
     data:{
         foodList:[
-            {name:'青枣',price:12,discount:0.5},
-            {name:'苹果',price:15,discount:0.7},
-            {name:'西红柿',price:8,discount:0.3},
+            {name:'肉包',price:3,discount:0.9},
+            {name:'三鲜包',price:5,discount:0.8},
+            {name:'菜包',price:10,discount:0.1},
         ]
     }
 });
